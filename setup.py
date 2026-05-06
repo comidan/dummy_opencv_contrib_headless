63166b5f4e21e9d4b27856a16a3941cf73cf3806
Daniele Comi <superdani96@gmail.com>
2026-05-06 12:45:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='opencv-contrib-python',
+    name='opencv-python-contrib',
     version='99.99.99', 
     install_requires=['opencv-python-contrib-headless'],
     description='Dummy package to intercept and redirect to headless opencv'
